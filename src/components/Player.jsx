bd38ace8eaa2669c18a54c292b976938c8237b87
stwhello <sherinpaul2001@gmail.com>
2026-02-25 08:53:10 +0530

    using useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,15 +7,13 @@ export default function Player() {
 
   function handleClick() {
     setPlayerName(input.current.value);
+    input.current.value = "";
   }
   return (
     <section id='player'>
       <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input
-          ref={input}
-          type='text'
-        />
+        <input ref={input} type='text' />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
