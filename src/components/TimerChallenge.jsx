ba0b5eb2296a21e679f2612d6f97446449e39135
stwhello <sherinpaul2001@gmail.com>
2026-02-26 08:56:55 +0530

    UI updated with conditional rendering

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
   return (
     <section className='challenge'>
-          <h2>{title}</h2>
-          <p className="challenge-time">
-              {targetTime} second{targetTime > 1 ? 's' : ''}
-          </p>
-          <p>
-              <button>
-                  Start Challenge
-              </button>
-          </p>
-          <p className="">
-              time is running.../timt is active
-          </p>
+      <h2>{title}</h2>
+      {timerExpired && <p>You Lost</p>}
+      <p className='challenge-time'>
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+       {timerStarted ? "Time is Running" : "Timer Inactive"}
+      </p>
     </section>
   );
 }
