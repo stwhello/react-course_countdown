6489f433b8a2de3a8200168a66765909182b8c2c
stwhello <sherinpaul2001@gmail.com>
2026-03-04 10:02:44 +0530

    sharing state across components

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,7 +1,11 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModel({ ref, result, targetTime }) {
+export default function ResultModel({ ref, targetTime, remainingTime, onReset }) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -12,14 +16,14 @@ export default function ResultModel({ ref, result, targetTime }) {
   });
   return (
     <dialog ref={dialog} className='result-modal'>
-      <h2>You {result}</h2>
+      {userLost && <h2>You Lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method='dialog'>
+      <form method='dialog' onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
